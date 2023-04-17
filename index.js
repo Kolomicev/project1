4d88eac33baded006dc1fc0401bcc4c338f5f9c3
Kolomicev <131021882+Kolomicev@users.noreply.github.com>
2023-04-17 21:11:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let name = 5;
+const leftBorderWidth = 1; 
+const obj = {
+     a: 10
+};
+
+obj.a = 5;
+console.log(obj);
+let userName = "John";
+let userNumber = 25;
+userNumber = 24;
+
+// let result = confirm('Are you here?');
+// console.log(result);
+// const answerAge = +prompt('How old are you?', '18');
+// console.log(answerAge + 5);
+
+// const answers = [];
+// answers[0] = prompt('What your name?', '');
+// answers[1] = prompt('What your Last name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+const category = 'toys';
+console.log(`https://someurl.com/${category}/29`);
